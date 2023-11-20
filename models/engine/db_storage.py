90a26367de611beddc03f2c8074d9702f032a9cd
roxxe34 <hamzafarissi7@gmail.com>
2023-11-20 23:27:51 +0100

    done

---
+++ models/engine/db_storage.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(getenv('HBNB_MYSQL_USER'),
+                                              getenv('HBNB_MYSQL_PWD'),
+                                              getenv('HBNB_MYSQL_HOST'),
+                                              getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        if cls is None:
+            objs = self.__session.query(User).all()
+            objs += self.__session.query(State).all()
+            objs += self.__session.query(City).all()
+            objs += self.__session.query(Amenity).all()
+        else:
+            objs = self.__session.query(cls).all()
+        return {'{}.{}'.format(type(obj).__name__, obj.id): obj for obj in objs}
+
+    def new(self, obj):
+        self.__session.add(obj)
+        self.save()
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj is not None:
+            self.__session.delete(obj)
+            self.save()
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import storage
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    @property
+    def cities(self):
+        """Getter attribute cities that returns the list of City instances with state_id equals State.id"""
+        all_cities = storage.all(City)
+        state_cities = []
+        for city in all_cities.values():
+            if city.state_id == self.id:
+                state_cities.append(city)
+        return state_cities
