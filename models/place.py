9d8896ebf28aff1361cbdec77fb4548e87af045d
roxxe34 <hamzafarissi7@gmail.com>
2023-11-22 13:33:43 +0100

    done

--- models/place.py
+++ models/place.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
-"""Defines the Place class."""
-import models
+""" Place Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, DateTime, create_engine, ForeignKey, Float, Table
+from sqlalchemy.orm import relationship
 from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
+import models
 from models.amenity import Amenity
 from models.review import Review
-from sqlalchemy import Column
-from sqlalchemy import Float
-from sqlalchemy import ForeignKey
-from sqlalchemy import Integer
-from sqlalchemy import String
-from sqlalchemy import Table
-from sqlalchemy.orm import relationship
-
 
 association_table = Table("place_amenity", Base.metadata,
                           Column("place_id", String(60),
@@ -25,26 +18,7 @@ association_table = Table("place_amenity", Base.metadata,
 
 
 class Place(BaseModel, Base):
-    """Represents a Place for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table places.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store places.
-        city_id (sqlalchemy String): The place's city id.
-        user_id (sqlalchemy String): The place's user id.
-        name (sqlalchemy String): The name.
-        description (sqlalchemy String): The description.
-        number_rooms (sqlalchemy Integer): The number of rooms.
-        number_bathrooms (sqlalchemy Integer): The number of bathrooms.
-        max_guest (sqlalchemy Integer): The maximum number of guests.
-        price_by_night (sqlalchemy Integer): The price by night.
-        latitude (sqlalchemy Float): The place's latitude.
-        longitude (sqlalchemy Float): The place's longitude.
-        reviews (sqlalchemy relationship): The Place-Review relationship.
-        amenities (sqlalchemy relationship): The Place-Amenity relationship.
-        amenity_ids (list): An id list of all linked amenities.
-    """
+    """ A place to stay """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
@@ -56,10 +30,10 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
+    amenity_ids = []
     reviews = relationship("Review", backref="place", cascade="delete")
     amenities = relationship("Amenity", secondary="place_amenity",
-                             viewonly=False)
-    amenity_ids = []
+                             viewonly=False, overlaps="place_amenities")
 
     if getenv("HBNB_TYPE_STORAGE", None) != "db":
         @property
